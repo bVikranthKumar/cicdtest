c225fb3e1ca11f198ac0650041eaa037e28635cb
bvikranthkumar <bvikranthkumar@aqfer.com>
2025-07-25 16:19:00 +0530

    updated central.sh

--- central.sh
+++ central.sh
@@ -1,7 +1,38 @@
-#! /bin/sh
+#!/usr/local/bin/bash
 
-echo "newlinejjj"
+declare -A PIPELINE_FILES_MAP # Declare associative array: pipeline → space-separated list of file patterns
+declare -A TRIGGERED_PIPELINES=() # Track triggered pipelines
 
-echo "lolzzhhsalso"
+PIPELINE_FILES_MAP["vikranth-cicd-pipeline-1"]="newdir/"
+PIPELINE_FILES_MAP["vikranth-cicd-pipeline-2"]="somefile.txt"
 
-echo "newline"
+
+# Get changed files in the latest commit
+CHANGED_FILES=$(git show --pretty="" --name-only HEAD)
+
+# Loop over pipelines and check their file patterns
+for pipeline in "${!PIPELINE_FILES_MAP[@]}"; do
+   patterns=${PIPELINE_FILES_MAP[$pipeline]}
+
+  for pattern in $patterns; do
+    for changed_file in ${CHANGED_FILES[@]}; do
+      if [[ "$changed_file" == "$pattern"* ]]; then
+        echo "File '$changed_file' matched pattern '$pattern' → $pipeline"
+        TRIGGERED_PIPELINES["$pipeline"]=1
+      fi
+    done
+  done
+done
+
+echo "pipelines to be triggered are: ${!TRIGGERED_PIPELINES[@]}"
+
+# Trigger each pipeline
+for pipeline in "${!TRIGGERED_PIPELINES[@]}"; do
+  echo "Triggering $pipeline..."
+#   aws codebuild start-build --project-name "$pipeline"
+done
+
+# # If nothing matched
+# if [[ ${#TRIGGERED_PIPELINES[@]} -eq 0 ]]; then
+#   echo "No matching changes. No pipelines triggered."
+# fi
